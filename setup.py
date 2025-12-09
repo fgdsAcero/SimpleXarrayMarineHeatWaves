ba21cf798c54bb95540e7901c3c94008383b019e
Franco Gael de Santiago Acero <103777911+fgdsAcero@users.noreply.github.com>
2025-12-09 13:37:30 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='SimpleXarrayMHWs',
+      version='0.01',
+      author = "Franco G. D. S. Acero",
+      author_email = "fgd2105@columbia.edu",
+      description = ("A set of scripts which closely implement the marine heatwave definition of Hobday et al. 2016 and the marine heatwave severity definition of Hobday et al. 2018."),
+      url = "https://github.com/fgdsAcero/SimpleXarrayMHWs",
+      license = "LICENSE.txt",
+      long_description = open('README.md').read(),
+      py_modules=['SimpleXarrayMHWs'],
+)

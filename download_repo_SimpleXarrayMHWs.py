eb415ba0ffd1cb0f660d096d3dc0384401b2fca6
Franco Gael de Santiago Acero <103777911+fgdsAcero@users.noreply.github.com>
2025-12-17 20:51:29 -0600

    Update download_repo_SimpleXarrayMHWs.py

--- download_repo_SimpleXarrayMHWs.py
+++ download_repo_SimpleXarrayMHWs.py
@@ -5,7 +5,7 @@ REPO_URL = 'https://github.com/fgdsAcero/SimpleXarrayMarineHeatWaves.git'
 LOCAL_DIR = './SimpleXarrayMarineHeatWaves'
 
 try:
-  print("Downloading the latest SimpleXarrayMHWs repository...")
+  print("Downloading the latest SimpleXarrayMarineHeatWaves repository...")
   Repo.clone_from(REPO_URL, LOCAL_DIR)
   print(f"✓ Successfully downloaded the repository to {LOCAL_DIR}!")
   print(f"✓ While comments are included in the scripts, it is recommended you check out the README.txt file.")
